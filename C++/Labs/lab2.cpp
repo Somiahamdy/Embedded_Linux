003e6c5945ec610d8844e4e449a0325555d88b04
Somia Soliman <89600235+Somiahamdy@users.noreply.github.com>
2024-07-07 16:23:23 +0300

    Add files via upload

--- C++/Labs/lab2.cpp
+++ C++/Labs/lab2.cpp
@@ -1,74 +1,46 @@
 #include <iostream>
-#include <stdlib.h>
-using namespace std;
+#include <math.h>
+#include <string>
 
-typedef struct {
-      int *arr;
-      int vsize;  //number of elements
-      int capacity; //size of whole array
-}vect;
 
-void InitArr(vect *vptr,int size){
-    vptr->arr=(int*)malloc(size*sizeof(int));
-    vptr->vsize=size;
-    vptr->capacity=size;
-    cout << "enter your values" << endl;
-    for(int i=0;i<size;i++){
-        cin >> vptr->arr[i];
+namespace math{
+    void print(double n){
+        std::cout << "result:" << sqrt(n) << std::endl;
     }
 }
-int Insertelement(vect* vptr,int elementnum,int data){
-    //check for element number limit
-    if(elementnum > vptr->capacity){
-        return -1;
-    }
-    //check for capacity 
-    if(vptr->capacity==vptr->vsize){
-        vptr->arr=(int*)realloc(vptr,vptr->vsize*2);
-        vptr->capacity=vptr->vsize*2;
-        vptr->vsize++;
-    }
-    for(int i=vptr->vsize;i>=elementnum;i--){
-        vptr->arr[i]=vptr->arr[i-1];
-        if(i==elementnum){
-            vptr->arr[elementnum]=data;
+namespace Stringfun{
+    void print(std::string s){
+        int size =sizeof(s);
+        std::cout << size;
+        for(int i=0;i<5;i++){
+         if(isupper(s[i])){
+            std::cout  << (char)tolower(s[i]);
+        }else if (std::islower(s[i])){
+            std::cout << (char)toupper(s[i]);
         }
-    }
+
+        }
  
-    return 1;
+    }
+    
 }
-
-int deleteelement(vect *vptr,int elementnum){
-    int* parr=(int*)malloc(vptr->vsize*sizeof(int));
-      //check for number limit
-      if(elementnum> vptr->vsize){
-        return -1;
-      }
-      for(int i=0;i<elementnum;i++){
-        if(i<elementnum){
-          parr[i]=vptr->arr[i];
-        }else if(i>=elementnum){
-            parr[i]=vptr->arr[i+1];
+namespace Array{
+    void print(int *ptr,int size){
+        for(int i=size-1;i>=0;i--){
+            std::cout << ptr[i] ;
         }
-      }
-      vptr->arr=parr;
-      vptr->vsize--;
-      return 1;
-}
-
-void printarr(vect* vptr){
-    for(int i=0;i<vptr->vsize;i++){
-        cout << "num " << vptr->arr[i] << endl;
     }
 }
 
 int main(){
-    vect v;
-    InitArr(&v,5);
-    printarr(&v);
-    Insertelement(&v,2,10);
-    printarr(&v);
-    deleteelement(&v,1);
-    printarr(&v);
+    
+    int num=9;
+    math::print(num);
+    std::string c="hello";
+    Stringfun ::print(c);
+    std:: cout << std::endl;
+    int arr[]={1,2,3,4,5};
+    Array::print(arr,5);
+    
     return 0;
 }
--- C++/Labs/lab3.cpp
+++ C++/Labs/lab3.cpp
@@ -1,47 +1,33 @@
 #include <iostream>
-#include <stdlib.h>
-using namespace std;
+#include <math.h>
+#include <string>
 
-int sum(int *subarr,int size){
-    int result=0;
-    for(int i=0;i<size;i++){
-        result+=subarr[i];
+namespace dynamic{
+    int** create2darray(int size){
+        int **ptr=new int*[size];
+        for(int i=0;i<size;i++){
+            ptr[i]=new  int[3];
+            for(int j=0;j<3;j++){
+                ptr[i][j]=1;
+            }
+        }
+        return ptr;
     }
-   return result;
-}
-typedef int* (*callfunc)(int **arr,int size,int* rowsize,int (*sumfunc)(int*,int));
-int* sumcall(int **parr,int si,int* r, int (*sf)(int* a,int siz)){
-    int *retarr;
-    for(int i=0;i<si;i++){
-        int result=sf(parr[i],r[i]);
-        retarr[i]=result;
-    }
-    return retarr;
-           
-}
-
-void arrprint(int *arr,int size){
-    for(int i=0;i<size;i++){
-        cout << "sum "<< i << " is: " << arr[i] << endl;
+    void deletearray(int **ptr){
+         delete (ptr);
     }
 }
 
 int main(){
-    int asize=3;
-    int a1[]={2,6,8,5};
-    int a2[]={10,5,9};
-    int a3[]={2,3,4,5,6,7};
+    int **array=dynamic::create2darray(3);
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            std::cout << array[i][j] ;
+        }
+        std::cout << std::endl;
+    }
+    dynamic::deletearray(array);
 
-    int s1=sizeof(a1)/sizeof(int);
-    int s2=sizeof(a2)/sizeof(int);
-    int s3=sizeof(a3)/sizeof(int);
-
-    int rowsize[]={s1,s2,s3};
-    int *fullarr[]={a1,a2,a3};
-    
-    int *retarr=sumcall(fullarr,asize,rowsize,sum);
-
-    arrprint(retarr,asize);
 
 
     return 0;
